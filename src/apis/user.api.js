2c26f71c2a6aafd70469531e30bcadde0a179a7a
Charlotte Germe <charlotte.germe16@gmail.com>
2025-03-07 15:29:04 +0100

    feat: page de connexion et d'inscription + back (non fonctionnel)

---
+++ src/apis/user.api.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const API_URL = 'https://pokemon-api-seyrinian-production.up.railway.app/users';
+
+
+export const loginUser = async (email, password)  => {
+    try {
+        const response = await axios.post(`https://pokemon-api-seyrinian-production.up.railway.app/users/login`, {
+            email,
+            password
+        });
+        console.log('Réponse de l’API :', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Erreur lors de la connexion :', error);
+        throw error;
+    }
+};
+
+export const createUser = async (email, password)  => {
+    console.log('Données envoyées:', email, password);
+
+    try {
+        const response = await axios.post(`https://pokemon-api-seyrinian-production.up.railway.app/users`, {
+            email,
+            password
+        });
+        console.log('Réponse de l’API :', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Erreur lors de la connexion :', error);
+        throw error;
+    }
+};
