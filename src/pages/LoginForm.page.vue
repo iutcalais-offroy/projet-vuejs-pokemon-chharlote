54ed527515106b5cdd659e583dfeb217eaa5b285
Charlotte Germe <charlotte.germe16@gmail.com>
2025-03-17 10:55:33 +0100

    feat: userStore avec gestion id et token

--- src/pages/LoginForm.page.vue
+++ src/pages/LoginForm.page.vue
@@ -1,76 +1,83 @@
 <script setup>
 import { ref } from 'vue';
 import { loginUser, createUser } from '../apis/user.api.js';
+import { useUserStore } from '../stores/userStore.js';
+import {useRouter} from "vue-router";
 
-// export default defineComponent({
-//   setup() {
-//     const valueRef = ref('')
-//     return {
-//       value: valueRef,
-//       options: computed(() => {
-//         return [
-//           ['Google', '@gmail.com'],
-//         ].map((emailInfo) => {
-//           return {
-//             type: 'group',
-//             label: emailInfo[0],
-//             key: emailInfo[0],
-//             children: [valueRef.value.split('@')[0] + emailInfo[1]]
-//           }
-//         })
-//       })
-//     }
-//   }
-// })
+
 
 const email = ref('');
 const mdp = ref('');
 const registerEmail = ref('');
 const registerMdp = ref('');
 const confirmMdp = ref('');
+const activeTab  = ref('Inscription');
 
+//
+// const Login = async () => {
+//   try {
+//     const response = await loginUser(
+//       email.value,
+//       mdp.value
+//     );
+//     console.log('Utilisateur connecté :', response);
+//   } catch (error) {
+//     console.error('Erreur de connexion :', error);
+//   }
+// };
+//
+// const Create = async () => {
+//   try {
+//     if (registerMdp.value !== confirmMdp.value) {
+//       console.error('Les mots de passe ne correspondent pas.');
+//       return;
+//     }
+//
+//     const response = await createUser(
+//         registerEmail.value,
+//         registerMdp.value
+//     );
+//     activeTab .value = 'Connexion';
+//     console.log('Utilisateur créé :', response);
+//   } catch (error) {
+//     console.error('Erreur lors de l’inscription :', error);
+//   }
+// };
 
-const Login = async () => {
-  try {
-    const response = await loginUser(
-      email.value,
-      mdp.value
-    );
-    console.log('Utilisateur connecté :', response);
-  } catch (error) {
-    console.error('Erreur de connexion :', error);
-  }
+  const userStore = useUserStore();
+
+  const Create = async () => {
+  if (registerMdp.value !== confirmMdp.value) {
+  console.error('Les mots de passe ne correspondent pas.');
+
+    // window.location.href = '/deck-builder';
+
+    return;
+}
+
+  await userStore.register(registerEmail.value, registerMdp.value);
 };
 
-const Create = async () => {
-  try {
-    if (registerMdp.value !== confirmMdp.value) {
-      console.error('Les mots de passe ne correspondent pas.');
-      return;
-    }
+  const Login = async () => {
+  await userStore.login(email.value, mdp.value);
 
-    const response = await createUser(
-        registerEmail.value,
-        registerMdp.value
-    );
-    console.log('Utilisateur créé :', response);
-  } catch (error) {
-    console.error('Erreur lors de l’inscription :', error);
-  }
+  activeTab.value= 'Connexion';
+
 };
-
 </script>
+
 
 <template>
   <n-card>
     <n-tabs
-        class="card-tabs"
-        default-value="signin"
-        size="large"
-        animated
-        pane-wrapper-style="margin: 0 -4px"
-        pane-style="padding-left: 4px; padding-right: 4px; box-sizing: border-box;"
+      v-model:value="activeTab "
+      class="card-tabs"
+      size="large"
+      animated
+      pane-wrapper-style="margin: 0 -4px"
+      pane-style="padding-left: 4px; padding-right: 4px; box-sizing: border-box;"
     >
+
       <n-tab-pane name="Connexion" tab="Connexion">
         <n-form>
           <n-form-item-row label="Email">
@@ -83,6 +90,7 @@ const Create = async () => {
         <n-button type="primary" block secondary strong @click="Login">
           Connexion
         </n-button>
+        <p>Pas de compte ? <a @click="activeTab = 'Inscription'">S'inscrire</a></p>
       </n-tab-pane>
 
       <n-tab-pane name="Inscription" tab="Inscription">
@@ -100,6 +108,7 @@ const Create = async () => {
         <n-button type="primary" block secondary strong @click="Create">
           Inscription
         </n-button>
+        <p>Déjà inscrit ? <a @click="activeTab = 'Connexion'">Se connecter</a></p>
       </n-tab-pane>
     </n-tabs>
   </n-card>
@@ -112,8 +121,9 @@ const Create = async () => {
   padding-left: 4px;
 }
 button {
-  background-color: lightskyblue;
+  background-color: #4CAF50;
   color: black;
   font-weight: bold;
 }
 </style>
+
